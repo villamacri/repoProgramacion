faa22dcfe14993c791c87f0e3b22c8d264a36710
Cristian Daniel Villalba <danvillalbamarin@gmail.com>
2025-04-30 11:00:40 +0200

    ejemplofechasbd

---
+++ EjemploFechasBd/src/main/java/com/salesianostriana/dam/ejemplofechasbd/EjemploFechasBdApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplofechasbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploFechasBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploFechasBdApplication.class, args);
+	}
+
+}
---
+++ EjemploFechasBd/src/test/java/com/salesianostriana/dam/ejemplofechasbd/EjemploFechasBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemplofechasbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploFechasBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
